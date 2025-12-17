8c3b03261cc97851ab097231ad9d1edd8df0c413
Abraham Ortiz <abrahamortizu@gmail.com>
2025-12-17 09:39:41 +0100

    feat: set up dependency injection

---
+++ src/infrastructure/di/container.ts
@@ -0,0 +1,39 @@
+import { Container } from "inversify";
+import { TYPES } from "./types";
+
+// Repository interfaces
+import { ICompanyRepository } from "@domain/repositories/company.repository";
+import { IInvoiceRepository } from "@domain/repositories/invoice.repository";
+import { ICardRepository } from "@domain/repositories/card.repository";
+import { ITransactionRepository } from "@domain/repositories/transaction.repository";
+
+// Repository implementations
+import {
+  CardRepository,
+  CompanyRepository,
+  InvoiceRepository,
+  TransactionRepository,
+} from "@infrastructure/database/repositories";
+
+export const container = new Container();
+
+// Register repositories
+container
+  .bind<ICompanyRepository>(TYPES.CompanyRepository)
+  .to(CompanyRepository)
+  .inSingletonScope();
+
+container
+  .bind<IInvoiceRepository>(TYPES.InvoiceRepository)
+  .to(InvoiceRepository)
+  .inSingletonScope();
+
+container
+  .bind<ICardRepository>(TYPES.CardRepository)
+  .to(CardRepository)
+  .inSingletonScope();
+
+container
+  .bind<ITransactionRepository>(TYPES.TransactionRepository)
+  .to(TransactionRepository)
+  .inSingletonScope();
---
+++ src/infrastructure/di/types.ts
@@ -0,0 +1,6 @@
+export const TYPES = {
+  CardRepository: Symbol.for("CardRepository"),
+  CompanyRepository: Symbol.for("CompanyRepository"),
+  InvoiceRepository: Symbol.for("InvoiceRepository"),
+  TransactionRepository: Symbol.for("TransactionRepository"),
+} as const;
